8e973d1869b8ed0aced09ba3c1a38d0108256d1a
daniel <gtdanniel@gmail.com>
2018-06-27 16:50:01 -0600

    GET method with ajax connected, locations inserted

--- admins/models.py
+++ admins/models.py
@@ -5,13 +5,22 @@ from django.db import models
 class Pais(models.Model):
     nombre = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.nombre
+
 class Departamento(models.Model):
     nombre = models.CharField(max_length=150)
     pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.nombre
+
 class Municipio(models.Model):
     nombre = models.CharField(max_length=150)
     Departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
 
 class Administrador(models.Model):
     nombres = models.CharField(max_length=100)
@@ -24,3 +33,6 @@ class Administrador(models.Model):
     nacimiento = models.DateField()
     cui = models.CharField(max_length=13)
     municipio = models.ForeignKey(Municipio, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellidos
